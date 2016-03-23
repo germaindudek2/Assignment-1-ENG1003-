3f1395fe686d36ba5e5535e8b674bd6afd27fc34
germaindudek2 <gdud2@student.monash.edu>
2016-03-23 14:49:21 +1100

    2nd edit

--- assignment1/assignment1/MorseCodeReceiver.js
+++ assignment1/assignment1/MorseCodeReceiver.js
@@ -27,19 +27,21 @@
 // ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
 
 
-/*
- * This function is called once per unit of time with camera image data.
- * 
- * Input : Image Data. An array of integers representing a sequence of pixels.
- *         Each pixel is representing by four consecutive integer values for 
- *         the 'red', 'green', 'blue' and 'alpha' values.  See the assignment
- *         instructions for more details.
- * Output: You should return a boolean denoting whether or not the image is 
- *         an 'on' (red) signal.
- */
+function redOrBlue(data) {
+			outputAreaRef.innerHTML += data;
+    }
+
+var dataTest = 0;
+var outputAreaRef = document.getElementById("messageField");
 function decodeCameraImage(data)
 {
-    // ADD YOUR CODE HERE
-
-    return false;
+    redOrBlue(data);
+    dataTest++;
+    if (dataTest ==2) {
+        while (dataTest != undefined) {
+           dataTest = 2; 
+        }
+    }
+}
+    }
 }
